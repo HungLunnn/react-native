2e1e5c4773232f704e49828c1ba0312a96eca924
HungLunnn <nguyenmanhhunglun@gmail.com>
2026-04-24 10:51:54 +0700

    video 4

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,16 +3,27 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-      <Text>OK</Text>
+      <View>
+        <Text style={styles.header}>hunglunn</Text>
+      </View>
+      <Text style={styles.title1}>src/app/index.tsx</Text>
+      <Text>OK nhaaaaaaa</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
+
+  title1: {
+    color: "red", fontSize: 50,
+    borderWidth: 1,
+    },
+    header: {
+      fontSize: 30,
+      fontWeight: 600
+    }
 });
