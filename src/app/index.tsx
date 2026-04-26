9743d6382038bebdc9e1c5aed4125d306ae25742
HungLunnn <nguyenmanhhunglun@gmail.com>
2026-04-26 23:40:25 +0700

    video 6

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,43 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
 
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<String>("");
+  const [age, setAge] = useState<number>(0);
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 30, fontWeight: "600"}}>
+      <View>
+        <Text style={{ fontSize: 30, fontWeight: "600" }}>Name: {name}</Text>
+        <TextInput
+          multiline
+          onChangeText={(value) => setName(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15
+          }} />
+      </View>
+      <View>
+        <Text style={{ fontSize: 30, fontWeight: "600" }}>Age: {age}</Text>
+        <TextInput
+          onChangeText={(value) => setAge(+value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15
+          }} />
+      </View>
+
+      <View>
+      <Text style={{ fontSize: 30, fontWeight: "600" }}>
           Count: {count}
         </Text>
-      <View>
-        <Button color={"red"} title="Increase" onPress={() => setCount(count + 1)}/>
+        <Button color={"red"} title="Increase" onPress={() => setCount(count + 1)} />
       </View>
     </View>
   );
